037dfb747b1c51a6a018959df8c1a8fa496ad483
Bozhidar Barbutov <cakovcenko4@gmail.com>
2026-02-11 16:00:29 +0200

    Create 07_water_overflow.py

---
+++ 02_Data_Types_and_Variables/Exercise/07_water_overflow.py
@@ -0,0 +1,9 @@
+number_of_lines = int(input())
+tank_capacity = 255
+for current_line in range(number_of_lines):
+    liters_of_water = int(input())
+    if liters_of_water > tank_capacity:
+        print("Insufficient capacity!")
+        continue
+    tank_capacity -= liters_of_water
+print(255 - tank_capacity)
