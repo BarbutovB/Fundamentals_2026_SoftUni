4bc13c90a36151246955663dd5944731801b7dff
Bozhidar Barbutov <cakovcenko4@gmail.com>
2026-02-11 15:54:12 +0200

    Create 04_sum_of_chars.py

---
+++ 02_Data_Types_and_Variables/Exercise/04_sum_of_chars.py
@@ -0,0 +1,6 @@
+number_of_characters = int(input())
+total_sum = 0
+for character in range(number_of_characters):
+    new_character = input()
+    total_sum += ord(new_character)
+print(f"The sum equals: {total_sum}")
