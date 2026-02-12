18766ef6d003f0ae45b397b07b6c957f47cba156
Bozhidar Barbutov <cakovcenko4@gmail.com>
2026-02-12 16:37:27 +0200

    Create 05_faro_shuffle.py

---
+++ 03_Lists_Basics/Exercise/05_faro_shuffle.py
@@ -0,0 +1,14 @@
+deck_of_cards = input().split()
+number_of_shuffles = int(input())
+for current_shuffle in range(number_of_shuffles):
+    middle_of_the_deck = len(deck_of_cards) // 2
+
+    shuffled_deck = []
+    left_part = deck_of_cards[:middle_of_the_deck]
+    right_part = deck_of_cards[middle_of_the_deck:]
+
+    for card_index in range(len(left_part)):
+        shuffled_deck.append(left_part[card_index])
+        shuffled_deck.append(right_part[card_index])
+    deck_of_cards = shuffled_deck.copy()
+print(shuffled_deck)
