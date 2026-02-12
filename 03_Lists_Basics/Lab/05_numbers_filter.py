b7c6832b16c1b074357b73f2259b771d74b99aaf
Bozhidar Barbutov <cakovcenko4@gmail.com>
2026-02-12 16:33:43 +0200

    Create 05_numbers_filter.py

---
+++ 03_Lists_Basics/Lab/05_numbers_filter.py
@@ -0,0 +1,26 @@
+n = int(input())
+numbers = []
+for _ in range(n):
+    current_number = int(input())
+    numbers.append(current_number)
+command = input()
+filtered_numbers = []
+if command == "even":
+    for num in numbers:
+        if num % 2 == 0:
+            filtered_numbers.append(num)
+if command == "odd":
+    for num in numbers:
+        if num % 2 != 0:
+            filtered_numbers.append(num)
+if command == "negative":
+    for num in numbers:
+        if num < 0:
+            filtered_numbers.append(num)
+if command == "positive":
+    for num in numbers:
+        if num >= 0:
+            filtered_numbers.append(num)
+
+print(filtered_numbers)
+
