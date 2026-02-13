93941f163a113e93bb365880c3bb3891fb7a706b
Bozhidar Barbutov <cakovcenko4@gmail.com>
2026-02-13 15:49:54 +0200

    Create 06_calculate_rectangle_area.py

---
+++ 04_Functions/Lab/06_calculate_rectangle_area.py
@@ -0,0 +1,13 @@
+def get_area(w, h):
+    return w * h
+
+width = int(input())
+height = int(input())
+
+print(get_area(width, height))def get_area(w, h):
+    return w * h
+
+width = int(input())
+height = int(input())
+
+print(get_area(width, height))
