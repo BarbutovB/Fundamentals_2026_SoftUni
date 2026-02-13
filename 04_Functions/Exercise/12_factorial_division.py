13f4e6d5f96e9dbc56b1e982a613536897e8330b
Bozhidar Barbutov <cakovcenko4@gmail.com>
2026-02-13 15:54:30 +0200

    Create 12_factorial_division.py

---
+++ 04_Functions/Exercise/12_factorial_division.py
@@ -0,0 +1,9 @@
+def factorial(n):
+    result = 1
+    for i in range(1, n + 1):
+        result *= i
+    return result
+
+num1, num2 = int(input()), int(input())
+res = factorial(num1) / factorial(num2)
+print(f"{res:.2f}")
