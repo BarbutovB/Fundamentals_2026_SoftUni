846d80841875c9fcceb5568b467876e92a4b45e7
Bozhidar Barbutov <cakovcenko4@gmail.com>
2026-03-20 17:09:31 +0200

    Create 02. Ad Astra.py

---
+++ Final_Exam_Preparation_2/02. Ad Astra.py
@@ -0,0 +1,11 @@
+import re
+ 
+text_string = input()
+pattern = r"(?i)([#|])([a-z\s]+)\1(\d{2}\/\d{2}\/\d{2})\1(\d+)\1"
+matches = re.findall(pattern, text_string)
+total_calories = sum([int(calories[3]) for calories in matches])
+days = total_calories // 2000
+print(f"You have food to last you for: {days} days!")
+for food in matches:
+    item_name, expiration_date, calories = food[1], food[2], food[3]
+    print(f"Item: {item_name}, Best before: {expiration_date}, Nutrition: {calories}")
